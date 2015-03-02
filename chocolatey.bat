52816a1cf66f0cbec72072ef559f272e05266c10
Felix Archambault <fel.archambault@gmail.com>
2015-03-02 09:41:22 -0500

    initial commit : list of interesting packages for a new ms windows install

---
+++ chocolatey.bat
@@ -0,0 +1,47 @@
+choco install git.install
+choco install notepadplusplus.install
+choco install firefox
+choco install 7zip.install
+
+:: python
+choco install python2
+choco install python
+choco install pywin32
+choco install easy.install
+choco install pip
+
+:: proxy
+choco install fiddler
+
+:: virtualisation
+choco install virtualbox
+choco install virtualbox.extensionpack
+choco install vagrant
+
+choco install wget
+
+choco install console2
+choco install curl
+choco install pdfcreator
+choco install conemu
+choco install cygwin
+choco install wireshark
+choco install winrar
+choco install google-chrome-x64
+
+
+choco install winpcap
+choco install foxitreader
+choco install pidgin
+choco install rdcman
+choco install cmder.portable
+choco install f.lux
+choco install grepwin
+choco install truecrypt
+choco install ctags
+choco install clover
+choco install procexp
+choco install synergy
+choco install miktex
+choco install linkshellextension
+choco install cyg-get
